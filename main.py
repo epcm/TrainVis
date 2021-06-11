0fdaac6ffd0a72adfaec4a78a0f379928907da0e
epcm <zhangkaiyuan999@outlook.com>
2021-06-12 00:51:10 +0800

    StationVis

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ app = Flask(__name__)
 # 解决乱码问题
 
 # 前端由此路由获取数据的json对象
-@app.route("/getData1", methods=['GET', 'POST'])
-def getData1():
-    js = ""
-    with open("static/PeopleInSubwayTime.json", "r", encoding='utf-8') as f:
-        js = f.read()
-    return js
+@app.route("/StationVis", methods=['GET', 'POST'])
+def StationVis():
+    return app.send_static_file("StationVis.html")
 
-@app.route("/getData2", methods=['GET', 'POST'])
-def getData2():
-    js = ""
-    with open("static/PeopleInSubwayCount.json", "r", encoding='utf-8') as f:
-        js = f.read()
-    return js
+@app.route("/TrainVis", methods=['GET', 'POST'])
+def TrainVis():
+    return app.send_static_file("TrainVis.html")
+
+@app.route("/AccessVis", methods=['GET', 'POST'])
+def AccessVis():
+    return app.send_static_file("AccessVis.html")
+
+@app.route("/PathVis", methods=['GET', 'POST'])
+def PathVis():
+    return app.send_static_file("PathVis.html")
 
 @app.route("/", methods=['GET', 'POST'])
 def root():
